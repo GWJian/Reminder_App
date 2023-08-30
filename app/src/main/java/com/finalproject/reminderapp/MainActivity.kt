b153e18bcd267f4346e365be4e080cf87ba3d43a
gwaijian <gwaijian@gmail.com>
2023-08-30 15:38:34 +0800

    testing

--- app/src/main/java/com/finalproject/reminderapp/MainActivity.kt
+++ app/src/main/java/com/finalproject/reminderapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.finalproject.reminderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//TEST
+//testing00
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
