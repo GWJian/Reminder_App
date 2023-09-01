2e5be57a9f8783abbd5fcc008a3a4d5a5cd477c9
Goh Wai Jian <gwaijian@gmail.com>
2023-09-01 15:56:02 +0800

    update Sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.finalproject.reminderapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.finalproject.reminderapp"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
