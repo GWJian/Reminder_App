5aebd87fbfc6ca548616a9a50e318166ea1272a6
JonesLim <tzercheng22l@forwardschool.edu.my>
2023-09-04 17:17:44 +0800

    implement update fragment

--- app/src/main/java/com/finalproject/reminderapp/ui/viewModels/UpdateRemindViewModel.kt
+++ app/src/main/java/com/finalproject/reminderapp/ui/viewModels/UpdateRemindViewModel.kt
@@ -1,7 +1,57 @@
 package com.finalproject.reminderapp.ui.viewModels
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.finalproject.reminderapp.MyApplication
+import com.finalproject.reminderapp.data.repo.RemindersRepo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class UpdateRemindViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UpdateRemindViewModel(
+    private val repo: RemindersRepo, private val savedStateHandle: SavedStateHandle
+) : BaseRemindViewModel() {
+    val id = savedStateHandle.get<Int>("reminderId") ?: -1
+
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            val res = repo.getRemindById(id)
+            res?.let {
+                title.value = it.title
+                desc.value = it.desc
+                date.value = it.date
+                time.value = it.time
+            }
+        }
+    }
+
+    fun update() {
+        val task = validateAndGetReminder()
+        task?.let {
+            viewModelScope.launch(Dispatchers.IO) {
+                repo.addRemind(it.copy(id))
+                finish.emit(Unit)
+            }
+        }
+    }
+
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val savedStateHandle = createSavedStateHandle()
+                val myRepository =
+                    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MyApplication).remindersRepo
+                UpdateRemindViewModel(
+                    repo = myRepository, savedStateHandle = savedStateHandle
+                )
+            }
+        }
+    }
+
+
 }
